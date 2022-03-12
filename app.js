d3d57b3e7a8bb7c4ffc76c0941ac28f8a8031a41
sj <stroudafk@gmail.com>
2022-03-12 15:45:03 -0600

    started countdown timer. have not finished testing

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 
 let intervalID;
 var timeDisplayed = document.getElementById("time");
-var timerValue = document.getElementById("toggleTimerBtnVal");
+var timerBtnValue = document.getElementById("toggleTimerBtnVal");
 var timerButton = document.getElementById("toggleTimerBtn");
 var timerState = false;
-
+var sessionStart;
+var sessionLength = 1200; // 20 minutes in seconds
+var timeElapsed = 0;
 
 function updateTime() {
-  timeDisplayed.innerHTML = Date();
+  timeElapsed += 1; // 1 = 0.1 seconds
+  let timeDifference = (sessionLength * 10) - timeElapsed;
+  let minutes = Math.floor((timeDifference / 10) / 60);
+  let seconds = Math.round((timeDifference / 10) % 60);
+  
+  if(timeElapsed >= 1 && timeElapsed <= 9){
+    timeDisplayed.innerHTML = sessionLength/60 + ":00";
+  }
+  else if(timeDifference === 0){
+    timeDisplayed.innerHTML = "0:00";
+  }
+  else if(seconds === 60){
+
+    timeDisplayed.innerHTML = (minutes) + ":59";
+  }
+  else if(seconds < 10){
+    timeDisplayed.innerHTML = minutes + ":0" + seconds;
+  }
+  else{
+    timeDisplayed.innerHTML = minutes + ":" + seconds;
+  }
+  
+
 }
 
 function startTimer(){
   if(!intervalID){
-    intervalID = setInterval(updateTime, 1000);
+    intervalID = setInterval(updateTime, 100);
   }
   timerState = true;
-  timerValue.innerHTML = "Stop";
+  timerBtnValue.innerHTML = "Stop";
+
 }
 
 function stopTimer(){
   clearInterval(intervalID);
   intervalID = null;
   timerState = false;
-  timerValue.innerHTML = "Start";
+  timerBtnValue.innerHTML = "Start";
 }
 
 function toggleTimer(){
