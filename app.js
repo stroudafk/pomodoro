35ab6996fa3bd2816c82c32fc1ab111d4c280c3e
sj <stroudafk@gmail.com>
2023-01-16 18:24:30 -0600

    changed time update to update once per second, fixed rounding error for 59-58 second jump

--- app.js
+++ app.js
@@ -9,20 +9,15 @@ var sessionLength = 1200; // 20 minutes in seconds
 var timeElapsed = 0;
 
 function updateTime() {
-  timeElapsed += 1; // 1 = 0.1 seconds
-  let timeDifference = (sessionLength * 10) - timeElapsed;
-  let minutes = Math.floor((timeDifference / 10) / 60);
-  let seconds = Math.round((timeDifference / 10) % 60);
+  let timeDifference = (sessionLength) - timeElapsed;
+  let minutes = Math.floor(timeDifference / 60);
+  let seconds = Math.ceil((timeDifference) % 60);
   
-  if(timeElapsed >= 1 && timeElapsed <= 9){
-    timeDisplayed.innerHTML = sessionLength/60 + ":00";
-  }
-  else if(timeDifference === 0){
+  if(timeDifference === 0){
     timeDisplayed.innerHTML = "0:00";
   }
   else if(seconds === 60){
-
-    timeDisplayed.innerHTML = (minutes) + ":59";
+    timeDisplayed.innerHTML = (minutes) + ":00";
   }
   else if(seconds < 10){
     timeDisplayed.innerHTML = minutes + ":0" + seconds;
@@ -30,13 +25,12 @@ function updateTime() {
   else{
     timeDisplayed.innerHTML = minutes + ":" + seconds;
   }
-  
-
+  timeElapsed += 1; // 1 = 1 second
 }
 
 function startTimer(){
   if(!intervalID){
-    intervalID = setInterval(updateTime, 100);
+    intervalID = setInterval(updateTime, 1000);
   }
   timerState = true;
   timerBtnValue.innerHTML = "Stop";
